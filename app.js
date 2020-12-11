355c46f1c734cba12d2022af1eeffdb66467ef57
Akash <tripathiakash387@gmail.com>
2020-12-11 21:47:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+let items = ["wake up", "writing shayries", "writing some code"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("us-EN", options);
+
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.NewItem;
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/Work");
+
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+})
+
+app.get("/Work", function(req, res) {
+  res.render("list", {
+    listTitle: "work List",
+    newListItems: workItems
+  })
+
+});
+
+app.post("/Work", function(req, res) {
+  let item = req.body.NewItem;
+  workItems.push(item);
+  res.redirect("/Work");
+})
+
+app.get("/about" ,function(req ,res){
+res.render("about");
+});
+
+
+app.listen(3000, function() {
+  console.log("sever is now live ");
+});
